64d19d926ffa076c2c2a1d6a6ad076ff8e55ac76
Kamata Kazuki <kizuka525@gmail.com>
2015-12-21 13:39:04 +0900

    ヘッダファイルを更新

--- include/GV.h
+++ include/GV.h
@@ -3,7 +3,31 @@
 #include <time.h>
 #include <string.h>
 
+#include <ncurses.h>
+#include <locale.h>
+
 #include "data.h" // データ構造関連
-#include "dice.h" // ダイス関連
-#include "control.h" // 操作関連
-#include "output.h"  // 情報表示関連
+#include "dice.h" // 六面ダイス関連
+#include "curses_init.h" // ncurses関連
+#include "print.h" // 表示と入力受付関連
+
+//#include "control.h" // 操作関連
+
+
+#define TRUE 1
+#define FALSE 0
+
+#define INIT_POWER 3
+#define INIT_HP 10
+#define INIT_DEFENSE 0
+#define INIT_RANK 0
+
+#define MAX_POWER 6
+#define MAX_HP 30
+#define MAX_DEFENSE 3
+
+#define DICE_MAX 6
+#define DICE_MIN 1
+
+#define BOARD_MAX 24
+#define EVENT_MAX 10
