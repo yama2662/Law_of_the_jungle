b61ad028a72cb35d3c1fa3dd17fa09f1ca843312
yama <yamada2662@gmail.com>
2015-12-21 20:28:04 +0900

    change_player_data.cをGV.hを利用する形で対応．ついでに最大値超え最小値未満となった場合に対応．

--- event/change_player_data.c
+++ event/change_player_data.c
@@ -1,50 +1,41 @@
-#include <stdio.h>
-#include <string.h>
+#include "../include/GV.h"
 
 #define N 20
 
-typedef struct eventList{
-  int powerVar;
-  int hpVar;
-  int defenseVar; // このイベントによる防御力の増減値
+void change_player_data(player_t *player_data,event_t *event){
+  //playerデータのpower,hp,defenseにevent分の変動値を加算
+  player_data->power += event->powerVar;
+  player_data->hp += event->hpVar;
+  player_data->defense += event->defenseVar;
 
-  char message[255]; // このイベントで表示するメッセージ
-}event_t;
-
-typedef struct pList{
-  char name[20];   // 名前
-  int playerNo;    // 番号
-
-  int power;       // 戦闘力
-  int hp;          // HP
-  int defense;     // 防御力
-  int rank;        // ゲーム終了時の順位
-
-  struct boardList *place; // 現在どのマスに止まっているか
-  struct pList *next; // 次のプレイヤー
-}player_t;
-
-typedef struct event{
-  int random_num;
-  int random_list[N];
-  event_t a[N];
-}event_data;
-
-void change_player_data(player_t *a,event_t *event){
-  a->power += event->powerVar;
-  a->hp += event->hpVar;
-  a->defense += event->defenseVar;
+  //以下それぞれの最大値超えを確認
+  //また，0以下になっていた場合も0へ修正する
+  if(player_data->power > MAX_POWER){
+    player_data->power = MAX_POWER;
+  }else if(player_data->power < 0){
+    player_data->power = 0;
+  }
+  if(player_data->hp > MAX_HP){
+    player_data->hp = MAX_HP;
+  }else if(player_data->hp < 0){
+    player_data->hp = 0;
+  }
+  if(player_data->defense >MAX_DEFENSE){
+    player_data->defense = MAX_DEFENSE;
+  }else if(player_data->defense < 0){
+      player_data->defense = 0;
+  }
 }
 
 int main(void){
   player_t player[4];
   event_data event;
 
-  event.a[0].hpVar = event.a[0].powerVar = event.a[0].defenseVar = 1;
+  event.fixed_event[0].hpVar = event.fixed_event[0].powerVar = event.fixed_event[0].defenseVar = 1;
   strcpy(player[0].name,"player1");
   player[0].hp = player[0].power = player[0].defense = 5;
 
-  change_player_data(&player[0],&event.a[0]);
+  change_player_data(&player[0],&event.fixed_event[0]);
   printf("%d\n%d\n%d\n",player[0].hp,player[0].power,player[0].defense);
   return 0;
 }
