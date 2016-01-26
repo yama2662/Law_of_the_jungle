8f4ec936cbe88724b1473bb0aea63602f83ebadc
hikacchi64 <higashi437_k11@yahoo.co.jp>
2016-01-26 14:30:04 +0900

    不要な変数の削除 ファイルポインタ名称変更 行数カウント関数の追加 イベントの数の変更に対応できるように変更

--- source/import_event_data.c
+++ source/import_event_data.c
@@ -4,13 +4,11 @@
 //å¼•æ•°ï¼šã‚¤ãƒ™ãƒ³ãƒˆãƒ‡ãƒ¼ã‚¿æ§‹é€ ä½“ã®ãƒã‚¤ãƒ³ã‚¿
 //æˆ»ã‚Šå€¤ï¼šãªã—
 void init_Event(event_data *event){
-  FILE *fp;
+  FILE *fp1;
   FILE *fp2;
   int i;
-  char c[256];
-  int d;
 
-    if((fp=fopen("fixed_event.txt","r"))==NULL){
+    if((fp1=fopen("fixed_event.txt","r"))==NULL){
       printf("fixed_event.txt can not open");
       exit(1);
     }
@@ -18,22 +16,24 @@ void init_Event(event_data *event){
       printf("random_event.txt can not open");
       exit(1);
       }
-    //å›ºå®šã‚¤ãƒ™ãƒ³ãƒˆã®ã‚»ãƒƒãƒˆ
-  for(i=0;i<FIXEDEVENT_MAX;i++){
-    fscanf(fp,"%d",&event->fixed_event[i].powerVar);
-    fscanf(fp,"%d",&event->fixed_event[i].hpVar);
-    fscanf(fp,"%d",&event->fixed_event[i].defenseVar);
-    fscanf(fp,"%s",&event->fixed_event[i].message);
+    fixedevent_max=fileline_check(fp1);//ŒÅ’èƒCƒxƒ“ƒg”ƒJƒEƒ“ƒg
+    randomevent_max=fileline_check(fp2);//ƒ‰ƒ“ƒ_ƒ€ƒCƒxƒ“ƒg”ƒJƒEƒ“ƒg
+    //ŒÅ’èƒCƒxƒ“ƒg‚ÌƒZƒbƒg
+  for(i=0;i<fixedevent_max;i++){
+    fscanf(fp1,"%d",&event->fixed_event[i].powerVar);
+    fscanf(fp1,"%d",&event->fixed_event[i].hpVar);
+    fscanf(fp1,"%d",&event->fixed_event[i].defenseVar);
+    fscanf(fp1,"%s",&event->fixed_event[i].message);
   }
-  //ãƒ©ãƒ³ãƒ€ãƒ ã‚¤ãƒ™ãƒ³ãƒˆã®ã‚»ãƒƒãƒˆ
-  for(i=0;i<RANDOMEVENT_MAX;i++){
+  //ƒ‰ƒ“ƒ_ƒ€ƒCƒxƒ“ƒg‚ÌƒZƒbƒg
+  for(i=0;i<randomevent_max;i++){
     fscanf(fp2,"%d",&event->random_list[i].powerVar);
     fscanf(fp2,"%d",&event->random_list[i].hpVar);
     fscanf(fp2,"%d",&event->random_list[i].defenseVar);
     fscanf(fp2,"%s",&event->random_list[i].message);
     }
 
-  fclose(fp);
+  fclose(fp1);
   fclose(fp2);
 }
 
@@ -44,11 +44,21 @@ void shuffle(event_data *event)
 {
   event_t tmp;
   int i,r;
-  //ãƒ©ãƒ³ãƒ€ãƒ ã‚¤ãƒ™ãƒ³ãƒˆã‚’ãƒ©ãƒ³ãƒ€ãƒ ã‚¤ãƒ™ãƒ³ãƒˆã®æ•°ã ã‘ã‚·ãƒ£ãƒƒãƒ•ãƒ«
-  for(i=0;i<RANDOMEVENT_MAX;i++){
-    r=rand()%RANDOMEVENT_MAX;//ä¹±æ•°
+  //ƒ‰ƒ“ƒ_ƒ€ƒCƒxƒ“ƒg‚ğƒ‰ƒ“ƒ_ƒ€ƒCƒxƒ“ƒg‚Ì”‚¾‚¯ƒVƒƒƒbƒtƒ‹
+  for(i=0;i<randomevent_max;i++){
+    r=rand()%randomevent_max;//—”
     tmp=event->random_list[i];
     event->random_list[i]=event->random_list[r];
     event->random_list[r]=tmp;
     }
+}
+
+int fileline_check(FILE *fp){
+  char dummy[LINE_MAX];
+  int line=0;
+  while ( fgets(dummy,LINE_MAX,fp) != NULL ) {
+        line++;
+    }
+fseek(fp, 0L, SEEK_SET);
+ return line;
 }
