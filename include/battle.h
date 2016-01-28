c3d8af9a3d95a1a7ceec0c34e022b4b2442d1477
IT1028 <suzu.sig.yu.nownon@gmail.com>
2016-01-28 19:52:34 +0900

    表示系関数宣言追加

--- include/battle.h
+++ include/battle.h
@@ -10,4 +10,22 @@ void battle(player_t *attack, player_t *defense);
 /* 反撃処理 (反撃される側, 反撃する側)*/
 void counter(player_t *attack, player_t *defense);
 
+/* 攻撃時のメッセージ */
+void print_battle_message(char a_name[], char d_name[], int a_offence);
+
+/* 攻撃成功時 */
+void print_battle_success(char d_name[], int before_hp, int after_hp);
+
+/* 攻撃失敗時 */
+void print_battle_failure();
+
+/* 反撃時 */
+void print_counter_message(char a_name[], char d_name[], int d_counter);
+
+/* 反撃成功時 */
+void print_counter_success(char a_name[], int before_pow);
+
+/* 反撃失敗時 */
+void print_counter_failure();
+
 #endif
