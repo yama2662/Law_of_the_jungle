cb82949683f27174ca15c6f9a2c01eeacc933e97
hikacchi64 <higashi437_k11@yahoo.co.jp>
2016-02-15 16:25:26 +0900

    O[oϐ̍폜 s(Cxg)JEg֐̍폜 LINE_MAX̍폜

--- source/import_event_data.c
+++ source/import_event_data.c
@@ -3,6 +3,7 @@
 //イベントデータを外部ファイルから読み込みイベントデータ構造体にセットする
 //引数：イベントデータ構造体のポインタ
 //戻り値：なし
+
 void init_Event(event_data *event){
   FILE *fp1;
   FILE *fp2;
@@ -16,17 +17,16 @@ void init_Event(event_data *event){
     printf("random_event.txt can not open");
     exit(1);
   }
-  fixedevent_max=fileline_check(fp1);//固定イベント数カウント
-  randomevent_max=fileline_check(fp2);//ランダムイベント数カウント
+
   //固定イベントのセット
-  for(i=0;i<fixedevent_max;i++){
+  for(i=0;i<FIXEDEVENT_MAX;i++){
     fscanf(fp1,"%d",&event->fixed_event[i].powerVar);
     fscanf(fp1,"%d",&event->fixed_event[i].hpVar);
     fscanf(fp1,"%d",&event->fixed_event[i].defenseVar);
     fscanf(fp1,"%s",&*event->fixed_event[i].message);
   }
   //ランダムイベントのセット
-  for(i=0;i<randomevent_max;i++){
+  for(i=0;i<RANDOMEVENT_MAX;i++){
     fscanf(fp2,"%d",&event->random_list[i].powerVar);
     fscanf(fp2,"%d",&event->random_list[i].hpVar);
     fscanf(fp2,"%d",&event->random_list[i].defenseVar);
@@ -45,20 +45,10 @@ void shuffle(event_data *event)
   event_t tmp;
   int i,r;
   //ランダムイベントをランダムイベントの数だけシャッフル
-  for(i=0;i<randomevent_max;i++){
-    r=rand()%randomevent_max;//乱数
+  for(i=0;i<RANDOMEVENT_MAX;i++){
+    r=rand()%RANDOMEVENT_MAX;//乱数
     tmp=event->random_list[i];
     event->random_list[i]=event->random_list[r];
     event->random_list[r]=tmp;
   }
-}
-
-int fileline_check(FILE *fp){
-  char dummy[LINE_MAX];
-  int line=0;
-  while ( fgets(dummy,LINE_MAX,fp) != NULL ) {
-    if(dummy[0]!='\n')line++;
-  }
-  fseek(fp, 0L, SEEK_SET);
-  return line;
 }
