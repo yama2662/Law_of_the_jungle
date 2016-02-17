479e936800bccd8fff905d5cba9905d9f0088059
ttyis <suzu.sig.yu.nownon@gmail.com>
2016-02-17 23:30:14 +0900

    反撃力0バグ解消

--- source/battle.c
+++ source/battle.c
@@ -34,7 +34,7 @@ void counter(player_t *attack, player_t *defense)
   int pip;
 
   /* 反撃する側の攻撃力の計算 */
-  counterattack = defense->hp / COUNTER_CONF;
+  counterattack = defense->hp / COUNTER_CONF + 1;
 
   print_counter_message(attack->name, defense->name, counterattack);
 
